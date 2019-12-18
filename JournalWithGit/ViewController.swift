39210ae1e50bbc9b6396fa55b6ec4033095467ec
Nathan Andrus <nathanpandrus@gmail.com>
2019-12-18 09:51:09 -0700

    Initial Commit

---
+++ JournalWithGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JournalWithGit
+//
+//  Created by Nathan Andrus on 12/18/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
